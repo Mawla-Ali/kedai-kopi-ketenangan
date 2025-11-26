1ee7574dfd0acd9924bc2bcb46fce517efbb9c3d
Mawla Ali <akungutku@gmail.com>
2025-11-26 13:59:48 +0700

    Menambahkan search bar

--- js/script.js
+++ js/script.js
@@ -2,18 +2,24 @@
 const navbarnav = document.querySelector(".navbar-nav");
 
 //Ketika humberger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarnav.classList.toggle("active");
+  e.preventDefault();
   // 1
 };
 
-//Klik diluar sidebar untuk menghilangkan nav
+//Klik diluar sidebar untuk mennutup elmemen
 // 2{
 const ham = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 document.addEventListener("click", function (e) {
   if (!ham.contains(e.target) && !navbarnav.contains(e.target)) {
     navbarnav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
   // }
 });
 
@@ -21,6 +27,12 @@ document.addEventListener("click", function (e) {
 const searchForm = document.querySelector(".search-form");
 const searchbox = document.querySelector("#search-box");
 
-document.querySelector("#search-button").onclick = () => {
+document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
+  //Agar pada saat di klik, search langsung aktif
+  searchbox.focus();
+  // Agar aksi ddefault tidak dilakukan. ketika mengaktifkan search
+  // di bagian tentang kami atau yang lain, pasti akan langsung kembali ke awal
+  // Agar mencegah hal tersebut, berikan perintah berikut
+  e.preventDefault();
 };
